78d9fc46824464ecf4976451193a7e98da0e6b4e
SourceCode <rrentfro@gmail.com>
2015-01-16 20:51:30 -0800

    Adds datastore abstract class

---
+++ system/abstract/datastore.php
@@ -0,0 +1,47 @@
+<?php
+/**
+* @package Strider
+* @author Ryan Rentfro & Isaac Mendoza
+* @license MIT
+*/
+namespace Strider;
+
+/**
+* The base datastore abstract class
+*/
+abstract class DataStore
+{
+    /**
+    * Stores the template data
+    */
+    protected static $dataStore = array();
+    
+    /**
+    * toArray() method requirement
+    */
+    abstract protected function toArray();
+    
+    /**
+    * Sets a template value
+    * @param $key The key to return the value for
+    * @return mixed|null Returns mixed value type, null if nothing found.
+    */
+    public static function getValue($key)
+    {
+        if (isset(self::$dataStore[$key])) return self::$dataStore[$key];
+        return null;  
+    }
+    
+    /**
+    * Sets a template value
+    * @param $key The key for the value to be added
+    * @param $value The value set for the key
+    * @return bool Returns true on completion
+    */
+    public static function setValue($key, $value)
+    {
+        self::$dataStore[$key] = $value;
+        return true;    
+    }
+}
+?>
