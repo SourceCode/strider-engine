b32cd9fe826d37258cc715fabe61a9e4d7cbf29a
SourceCode <rrentfro@gmail.com>
2015-01-16 20:55:15 -0800

    Updates template adding datastore abstract class

--- system/core/template.php
+++ system/core/template.php
@@ -9,13 +9,9 @@ namespace Strider;
 /**
  * The base Template class
  */
-class Template
+class Template extends DataStore
 {
     use Singleton;
-    /**
-    * Stores the template data
-    */
-    private static $dataStore = array();
     
     /**
     * Stores JavaScript includes for the template system
@@ -27,28 +23,7 @@ class Template
     */
     private static $cssIncludes = array();
     
-    /**
-    * Sets a template value
-    * @param $key The key to return the value for
-    * @return mixed|null Returns mixed value type, null if nothing found.
-    */
-    public static function getValue($key)
-    {
-        if (isset(self::$dataStore[$key])) return self::$dataStore[$key];
-        return null;  
-    }
-    
-    /**
-    * Sets a template value
-    * @param $key The key for the value to be added
-    * @param $value The value set for the key
-    * @return bool Returns true on completion
-    */
-    public static function setValue($key, $value)
-    {
-        self::$dataStore[$key] = $value;
-        return true;    
-    }
+
     
     /**
     * Checks if a value exists
