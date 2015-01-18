13632cc76f913d6c52aa971aee0d58929bb28cae
SourceCode <rrentfro@gmail.com>
2015-01-17 16:44:35 -0800

    Updates the globals file adding url parsing

--- system/core/global.php
+++ system/core/global.php
@@ -44,6 +44,26 @@ class Globals extends DataStore
     private static $files = array();
     
     /**
+    * Contains the current requested app from the URL
+    */
+    private static $app = '';
+    
+    /**
+    * Contains the current requested controller from the URL
+    */
+    private static $controller = '';
+    
+    /**
+    * Contains the current requested module from the URL
+    */
+    private static $module;
+    
+    /**
+    * CContains the current parameters from the URL
+    */
+    private static $parm = array();
+    
+    /**
     * Constructs the object setting its properties.
     */
     public function __construct()
@@ -55,8 +75,33 @@ class Globals extends DataStore
         if (isset($_POST)) self::$post = $_POST;    
         if (isset($_REQUEST)) self::$request = $_REQUEST;    
         if (isset($_FILES)) self::$files = $_FILES;
-        self::setGlobal('URL', Config::$webPath);
-        self::setGlobal('appTitle', Config::$projectName); 
+        self::setValue('URL', Config::$webPath);
+        self::setValue('appTitle', Config::$projectName);
+        self::parseUrl(); 
+    }
+    
+    /**
+    * Parse URL into app, controller, module, and 9 acceptable URL parameter slots.
+    * Additional parameters should be consider for placement into session or ajax/web service call.
+    */
+    private static function parseUrl()
+    {
+        if (isset($_GET['url'])) {
+            $url = filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            self::$app = (isset($url[0]) ? $url[0] : null);
+            self::$controller = (isset($url[1]) ? $url[1] : null);
+            self::$module = (isset($url[2]) ? $url[2] : null);
+            self::$parm[1] = (isset($url[3]) ? $url[3] : null);
+            self::$parm[2] = (isset($url[4]) ? $url[4] : null);
+            self::$parm[3] = (isset($url[5]) ? $url[5] : null);
+            self::$parm[4] = (isset($url[6]) ? $url[6] : null);
+            self::$parm[5] = (isset($url[7]) ? $url[7] : null);
+            self::$parm[6] = (isset($url[8]) ? $url[8] : null);
+            self::$parm[7] = (isset($url[9]) ? $url[9] : null);
+            self::$parm[8] = (isset($url[10]) ? $url[10] : null);
+            self::$parm[9] = (isset($url[11]) ? $url[11] : null);
+        }
     }
     
     /**
@@ -119,7 +164,53 @@ class Globals extends DataStore
     */
     public static function files()
     {
-        return self::files();
+        return self::files;
+    }
+    
+    /**
+    * Returns the current requested app
+    * @return string Returns the current requested app
+    */
+    public static function app()
+    {
+        return self::app;
+    }
+    
+    /**
+    * Returns the current requested controller
+    * @return string Returns the current requested controller
+    */
+    public static function controller()
+    {
+        return self::controller;
+    }
+    
+    /**
+    * Returns the current requested module
+    * @return string Returns the current requested module
+    */
+    public static function module()
+    {
+        return self::module;
+    }
+    
+    /**
+    * Returns the current requested parameters
+    * @return array Returns the current requested parameters
+    */
+    public static function parm()
+    {
+        return self::parm;
+    }
+    
+    /**
+    * Get parameter
+    * @return array|null Returns the requested paramter by key or null if nothing found
+    */
+    public static function getParm($key)
+    {
+        if (is_numeric($key) && isset($parm[$key])) return self::parm[$key];
+        return null;
     }
 
     /**
@@ -136,6 +227,10 @@ class Globals extends DataStore
         $array['post'] = self::post();
         $array['request'] = self::request();
         $array['files'] = self::files();
+        $array['app'] = self::app();
+        $array['controller'] = self::controller();
+        $array['module'] = self::module();
+        $array['parm'] = self::parm();
         return $array;
     }
 }
